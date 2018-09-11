0e1453df83db43d906596d66397cb42a6bd048dd
Ardus <artem.werwein@googlemail.com>
2018-09-11 18:08:34 +0200

    queue exception handling

--- queue/queue.c
+++ queue/queue.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-
-//no exception handling!!!
 
 typedef struct {
   int n;
   int front;
   int rear;
   int *array;
+  int isFull;
+  int isEmpty;
 }Queue;
 
 Queue* initQ(int size){
@@ -17,42 +18,62 @@ Queue* initQ(int size){
   queue->front = 0;
   queue->rear = 0;
   queue->array = malloc(size*sizeof(int));
+  queue->isFull= 0;
+  queue->isEmpty = 1;
   // printf("initialized queue\n");
   return queue;
 }
 
 void enque(Queue *q, int i){
-  // printf("%d\n", q->rear);
-  q->array[q->rear] = i;
-  printf("enqued %d at Queue[%d]\n", i, q->rear);
-  if (q->rear == q->n - 1) {
-    q->rear = 0;
+  if (!q->isFull) {
+    // printf("%d\n", q->rear);
+    q->array[q->rear] = i;
+    printf("enqued %d at Queue[%d]\n", i, q->rear);
+    if (q->rear == q->n - 1) {
+      q->rear = 0;
+    }
+    else {
+      q->rear = q->rear + 1;
+    }
+    if (q->rear == q->front) {
+      q->isFull = 1;
+    }
+    q->isEmpty = 0;
+    return;
+  } else {
+    printf("queue is full\n");
   }
-  else {
-    q->rear = q->rear + 1;
-  }
-  return;
 }
+
 
 int dequeue(Queue *q){
-  int out = q->array[q->front];
-  if (q->front == q->n -1) {
-    q->front = 0;
-  } else {
-    q->front = q->front +1;
+  if (!q->isEmpty) {
+    int out = q->array[q->front];
+    if (q->front == q->n -1) {
+      q->front = 0;
+    } else {
+      q->front = q->front +1;
+    }
+    printf("dequed %d\n", out);
+    if (q->rear == q->front) {
+      q->isEmpty = 1;
+    }
+    q->isFull = 0;
+    return out;
   }
-  printf("dequed %d\n", out);
-  return out;
+  else {
+    printf("Queue is empty\n");
+  }
 }
-
 
 
 
 int main(int argc, char const *argv[]) {
 
   printf("init\n");
-  Queue* Q = initQ(10);
+  Queue* Q = initQ(2);
   // printf("enqued\n");
+  dequeue(Q);
   enque(Q, 1);
   enque(Q, 24);
   dequeue(Q);
